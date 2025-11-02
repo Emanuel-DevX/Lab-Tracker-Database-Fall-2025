90c7a74847b6f9484a8c31d28a52ab46387d836f
Emanuel-DevX <jefymoxi@gmail.com>
2025-11-02 15:28:48 -0800

    Add Set Table

--- schema.sql
+++ schema.sql
@@ -5,10 +5,16 @@ SET search_path TO lab_tracker_group_16;
 --Drop tables before definition
 
 DROP TABLE IF EXISTS Course CASCADE;
+DROP TABLE IF EXISTS Set CASCADE;
 
 CREATE TABLE Course (
     course_code CHAR(8) PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     credits INTEGER NOT NULL
 
+);
+
+CREATE TABLE Set (
+    set_code CHAR(1) PRIMARY KEY,
+    campus VARCHAR(50) NOT NULL
 );
