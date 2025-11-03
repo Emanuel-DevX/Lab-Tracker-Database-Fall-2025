e6c76a428f0817efd7d9f00e4c0cb6226292e095
Emanuel-DevX <jefymoxi@gmail.com>
2025-11-02 17:07:56 -0800

    Insert Into term

--- seed_data.sql
+++ seed_data.sql
@@ -18,4 +18,11 @@ CASCADE;
 INSERT INTO course (course_code, title, credits) VALUES
 ('COMP2714','Relational Database Systems',3);
 
-SELECT * FROM course;
+
+INSERT INTO term (term_code, name, start_date, end_date) VALUES
+('202510', 'Winter 2025', '2025-01-06', '2025-04-11'),
+('202520', 'Spring/Summer 2025', '2025-04-28', '2025-08-08'),
+('202530', 'Fall 2025', '2025-09-02', '2025-12-12');
+
+SELECT * FROM term;
+
