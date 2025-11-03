29825b6a6ec2026c7dec3ddeeb10323e6a9599f8
Allen Rosales <arosales9@my.bcit.ca>
2025-11-02 17:06:53 -0800

    Insert into set table

--- seed_data.sql
+++ seed_data.sql
@@ -19,3 +19,12 @@ INSERT INTO course (course_code, title, credits) VALUES
 ('COMP2714','Relational Database Systems',3);
 
 SELECT * FROM course;
+
+INSERT INTO set (set_code, campus) VALUES ('A', 'Burnaby');
+INSERT INTO set (set_code, campus) VALUES ('B', 'Burnaby');
+INSERT INTO set (set_code, campus) VALUES ('C', 'Burnaby');
+INSERT INTO set (set_code, campus) VALUES ('D', 'Burnaby');
+INSERT INTO set (set_code, campus) VALUES ('E', 'Downtown');
+INSERT INTO set (set_code, campus) VALUES ('F', 'Downtown');
+
+SELECT * FROM set;
